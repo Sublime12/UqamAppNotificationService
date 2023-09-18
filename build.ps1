f2f75a064515edbc106dd430a01215359bb77d4c
Sublime12 <tshimangasublime@gmail.com>
2023-09-17 23:30:54 -0400

    Add publish script for windows

--- build.ps1
+++ build.ps1
@@ -1,1 +1,9 @@
-dotnet publish --sc --os win
+rmdir build -Recurse -Force
+
+echo "Building for Windows"
+echo "First, install the dotnet sdk"
+
+dotnet publish --sc --os win -c Production -o ./build/windows/
+
+echo "Then create a file named appsettings.json in the build/windows folder and change the PermanentCode and the Password to your own values"
+
